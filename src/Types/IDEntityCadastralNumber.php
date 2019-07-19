9bb099466c9345a5fae6b06ef7e178477bd946ed
eldario <eldario666@gmail.com>
2019-07-19 15:53:42 +0500

    Update with region codes

--- src/Types/IDEntityCadastralNumber.php
+++ src/Types/IDEntityCadastralNumber.php
@@ -5,9 +5,11 @@ declare(strict_types = 1);
 namespace AvtoDev\IDEntity\Types;
 
 use Exception;
+use AvtoDev\StaticReferences\References\AutoRegions\AutoRegions;
+use AvtoDev\StaticReferences\References\AutoRegions\AutoRegionEntry;
 use AvtoDev\ExtendedLaravelValidator\Extensions\CadastralNumberValidatorExtension;
 
-class IDEntityCadastralNumber extends AbstractTypedIDEntity
+class IDEntityCadastralNumber extends AbstractTypedIDEntity implements HasRegionDataInterface
 {
     /**
      * {@inheritdoc}
@@ -23,14 +25,43 @@ class IDEntityCadastralNumber extends AbstractTypedIDEntity
     public static function normalize($value): ?string
     {
         try {
-            // Удаляем все символы, кроме ./разрешенных
-            $value = (string) \preg_replace('~[^\d\:]~u', '', (string) $value);
-
-            // Удаляем первые и последние символы, если это не цифры
-            return \trim($value, ':');
+            // Удаляем все символы, кроме разрешенных (цифры и знак ":")
+            return (string) \preg_replace('~[^\d\:]~u', '', (string) $value);
         } catch (Exception $e) {
             return null;
         }
+    }
+
+    /**
+     * Возвращает код региона.
+     *
+     * @return int|null
+     */
+    public function getRegionCode(): ?int
+    {
+        if (\preg_match('~^(?<region_code>[0-9]{2})~', (string) $this->value, $matches)) {
+            if (isset($matches['region_code']) && ! \trim($region_code = $matches['region_code']) !== '') {
+                return (int) $region_code;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Возвращает данные региона по коду региона кадастрового номера (первые 2 цифры).
+     *
+     * @return AutoRegionEntry|null
+     */
+    public function getRegionData(): ?AutoRegionEntry
+    {
+        static $regions = null;
+
+        if (! $regions instanceof AutoRegions) {
+            $regions = new AutoRegions;
+        }
+
+        return $regions->getByRegionCode($this->getRegionCode());
     }
 
     /**
@@ -41,6 +72,15 @@ class IDEntityCadastralNumber extends AbstractTypedIDEntity
         /** @var CadastralNumberValidatorExtension $validator */
         $validator = static::getContainer()->make(CadastralNumberValidatorExtension::class);
 
-        return \is_string($this->value) && $validator->passes('', $this->value);
+        $validated = \is_string($this->value) && $validator->passes('', $this->value);
+
+        $region_valid = false;
+
+        // Пропускаем проверку формата, в котором в принципе нет кода региона
+        if ($this->getRegionData() instanceof AutoRegionEntry) {
+            $region_valid = true;
+        }
+
+        return $validated && $region_valid;
     }
 }
